35609697e1abbcc64e934c69f1f2eb7b473b56e3
Antonio <108486153+anGl4@users.noreply.github.com>
2022-07-16 12:07:21 +0200

    Add files via upload

---
+++ JpgFileVisitor.java
@@ -0,0 +1,42 @@
+package swingJpg;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JpgFileVisitor extends SimpleFileVisitor<Path> {
+	Map<Path, String> data = new HashMap<>();
+
+	@Override
+	public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+		String fileName = file.getFileName().toString();
+
+		if (fileName.endsWith(".jpg")) {
+			data.put(file.toAbsolutePath(), parseTitle(fileName));
+		}
+
+		return FileVisitResult.CONTINUE;
+	}
+
+	public String parseTitle(String pathName) {
+		return pathName.substring(0, pathName.indexOf("."));
+	}
+
+	public Map<Path, String> getData() {
+		return data;
+	}
+
+//	public static void main(String[] args) throws IOException {
+//		Path path = Path.of("C:\\VariousData");
+//		JpgFileVisitor visitor = new JpgFileVisitor();
+//		Files.walkFileTree(path, visitor);
+//		Map<Path, String> files = visitor.getData();
+//		for (Map.Entry<Path, String> entry : files.entrySet()) {
+//			System.out.println(entry.getKey().toString() + ", title: " + entry.getValue());
+//		}
+//	}
+}
---
+++ JpgFrame.java
@@ -0,0 +1,153 @@
+package swingJpg;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
+import javax.swing.WindowConstants;
+
+public class JpgFrame extends JFrame {
+
+	JpgFileVisitor visitor = new JpgFileVisitor();
+	// Path path = Path.of("C:\\VariousData");
+	Path path = Path.of("");
+
+	JButton btnDownload = new JButton("Download");
+	JProgressBar progressBar = new JProgressBar(SwingConstants.HORIZONTAL);
+	JTextField statusField = new JTextField();
+	JLabel picture = new JLabel();
+	JTextField searchField = new JTextField();
+	JLabel lblSearch = new JLabel("Enter the path you want to visit:");
+
+	public JpgFrame() throws IOException {
+		setLayout();
+		setFunctionality();
+	}
+
+	public void setFunctionality() throws IOException {
+		btnDownload.addActionListener((e) -> {
+			String searchString = searchField.getText();
+			Path searchPath = Path.of(searchString);
+			if (!searchPath.toFile().exists()) {
+				statusField.setText("You've entered the wrong path");
+			} else {
+				reset();
+				path = searchPath;
+				JpgWorker worker = new JpgWorker();
+				btnDownload.setEnabled(false);
+				worker.execute();
+			}
+		});
+	}
+
+	public void reset() {
+		progressBar.setValue(0);
+		statusField.setText("");
+	}
+
+	public void setLayout() {
+		// North panel
+		JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 25, 5));
+		searchField.setColumns(30);
+		northPanel.add(lblSearch);
+		northPanel.add(searchField);
+		northPanel.add(btnDownload);
+		add(northPanel, BorderLayout.NORTH);
+
+		// South panel
+		JPanel southPanel = new JPanel(new BorderLayout());
+		statusField.setEditable(false);
+		progressBar.setStringPainted(true);
+		southPanel.add(progressBar, BorderLayout.NORTH);
+		southPanel.add(statusField, BorderLayout.SOUTH);
+		add(southPanel, BorderLayout.SOUTH);
+
+		// Center
+		picture.setPreferredSize(new Dimension(600, 340));
+		JPanel midPanel = new JPanel();
+		midPanel.add(picture);
+		add(midPanel, BorderLayout.CENTER);
+	}
+
+	class JpgWorker extends SwingWorker<String, String> {
+		int counter = 0;
+		int size = 0;
+
+		@Override
+		protected String doInBackground() throws Exception {
+			Files.walkFileTree(path, visitor);
+			Map<Path, String> data = visitor.getData();
+			size = data.keySet().size();
+
+			progressBar.setMaximum(size);
+
+			for (Path p : data.keySet()) {
+				ImageIcon icon = loadImage(p);
+				picture.setIcon(icon);
+				publish(data.get(p));
+			}
+			return "Download completed!";
+		}
+
+		@Override
+		protected void process(List<String> chunks) {
+			for (String s : chunks) {
+				statusField.setText(s);
+				progressBar.setValue(++counter);
+			}
+		}
+
+		@Override
+		protected void done() {
+			try {
+				statusField.setText(this.get());
+			} catch (InterruptedException | ExecutionException e) {
+				System.err.println("Something went wrong");
+			}
+			progressBar.setValue(0);
+			btnDownload.setEnabled(true);
+		}
+
+	}
+
+	public ImageIcon loadImage(Path p) throws InterruptedException {
+		ImageIcon icon = new ImageIcon(p.toString());
+		Image image = icon.getImage().getScaledInstance(600, 340, Image.SCALE_DEFAULT);
+		Thread.sleep(2500);
+		return new ImageIcon(image);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			JpgFrame frame;
+			try {
+				frame = new JpgFrame();
+				frame.setTitle("JPG Loader");
+				frame.setLocation(150, 50);
+				frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+				frame.setSize(720, 500);
+				frame.setVisible(true);
+			} catch (IOException e) {
+				System.err.println("Aw hell naw");
+			}
+
+		});
+	}
+}
